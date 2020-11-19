1f06dea31551c1c071376bedde383531399ecb55
coskun cakir <csknckr@gmail.com>
2020-11-19 16:58:49 +0000

    fix: sitemetada hook

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -8,7 +8,7 @@ const useSiteMetadata = () => {
           title
           titleTemplate
           description
-          url
+          siteUrl
           image
           twitterUsername
         }
